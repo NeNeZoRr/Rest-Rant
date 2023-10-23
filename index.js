f51bf728b46853a57119f2e2203dab63e51ed668
NeNeZoRr <zorrillalin@gmail.com>
2023-10-23 15:08:27 -0400

    Adding remaining stub routes 	new file:   README.md 	modified:   index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const placesRouter = require('./Controllers/places');
 
-app.use('/places', require('./controllers/places'))
+app.use('/places', placesRouter);
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+    res.send('Finally!');
+});
 
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-})
+app.use((req, res, next) => {
+    res.status(404).send('<h1>404 Page</h1>');
+});
 
-app.listen(process.env.PORT)
+const port = process.env.PORT
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
