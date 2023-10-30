94c6cc9ba14a39701cfe3983549b204ee0a9d151
NeNeZoRr <zorrillalin@gmail.com>
2023-10-30 13:25:11 -0400

    Styling

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -11,22 +11,12 @@ function show(data) {
                     </div>
                     <div className="col-sm-6">
                         <h1>{data.place.name}</h1>
-                        <h2>
-                            Rating
-                        </h2>
-                        <h3 className="inactive">
-                            Not Rated
-                        </h3>
+                        <h2>Rating</h2>
+                        <h3 className="inactive">Not Rated</h3>
                         <br />
-                        <h2>
-                            Description
-                        </h2>
-                        <h3>
-                            Located in {data.place.city}, {data.place.state}
-                        </h3>
-                        <h3>
-                            Serving {data.place.cuisines}
-                        </h3>
+                        <h2> Description</h2>
+                        <h3> Located in {data.place.city}, {data.place.state}</h3>
+                        <h3> Serving {data.place.cuisines} </h3>
                         <br />
                         <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                             Edit
