3767819951d537c16a87f1731f06b7f048d84e4c
NeNeZoRr <zorrillalin@gmail.com>
2023-10-30 10:49:38 -0400

    Delete Functionality Implemented

--- Controllers/places.js
+++ Controllers/places.js
@@ -33,28 +33,69 @@ router.get('/:id', (req, res) => {
         res.render('error404')
     }
     else {
-        res.render('places/show', { place: places[id] })
+        res.render('places/show', { place: places[id], id })
     }
 })
 
 router.put('/:id', (req, res) => {
-    res.send('PUT /places/:id')
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = '/images/Error404.jpg'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+        // Save the new data into places[id]
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
+    }
 })
 
 router.delete('/:id', (req, res) => {
-    res.send('DELETE /places/:id')
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
 })
 
 router.get('/:id/edit', (req, res) => {
-    res.send('GET /places/:id/edit')
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/edit', { place: places[id], id })
+    }
 })
 
 router.post('/:id/rant', (req, res) => {
-    res.send('GET /places/:id/rant')
+    res.send('GET /places/:id/rant stub')
 })
 
 router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId')
+    res.send('GET /places/:id/rant/:rantId stub')
 })
 
 
