cb848a804e0f2fcf1c6a1c34147cfe4f22ab7fa4
NeNeZoRr <zorrillalin@gmail.com>
2023-11-08 13:15:10 -0500

    troubleshooting

--- models/places.js
+++ models/places.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  pic: String,
-  cuisines: { type: String, required: true },
-  city: { type: String, default: 'Anytown' },
-  state: { type: String, default: 'USA' },
-  founded: Number
+    name: { type: String, required: true },
+    pic: String,
+    cuisines: { type: String, required: true },
+    city: { type: String, default: 'Anytown' },
+    state: { type: String, default: 'USA' },
+    founded: Number
 })
+
+placeSchema.methods.showEstablished = function () {
+    return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
+}
 
 module.exports = mongoose.model('Place', placeSchema)
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,37 +1,58 @@
 const React = require('react')
-const Def = require('../default')
+const Def = require('../Default')
 
 function show(data) {
+    let comments = (
+        <h3 className='inactive'>No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <main>
                 <div className="row">
-                        <img style={{ width: 450, height: 450 }} className="Showpic" src={data.place.pic} alt={data.place.name} />
+                    <div className="col-sm-6">
+                        <img style={{ width: 460, height: 500 }} className="pic" src={data.place.pic} alt={data.place.name} />
+                    </div>
+                    <h3>
+                        Located in {data.place.city}, {data.place.state}
+                    </h3>
                     <div className="col-sm-6">
                         <h1>{data.place.name}</h1>
-                        <h2>Rating</h2>
-                        <h3 className="inactive">Not Rated</h3>
+                        <h2>Comments</h2>
+                        {comments}
                         <br />
                         <h2> Description</h2>
-                        <h3> Located in {data.place.city}, {data.place.state}</h3>
-                        <h3> Serving {data.place.cuisines} </h3>
+                        <h3>
+                            {data.place.showEstablished()}
+                        </h3>
+                        <h4>
+                            Serving {data.place.cuisines}
+                        </h4>
                         <br />
-                        <div className="actionbtn">
-                            <div className="yellowbtn">
-                            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                                Edit
-                            </a>{` `}
-                        </div>
-                        <div className="redbtn">
-                            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                                <button type="submit" className="btn btn-danger">
-                                    Delete
-                                </button>
-                            </form>
-                            </div>
-                            </div>
-                        </div>
+                        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                            Edit
+                        </a>{` `}
+                        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                            <button type="submit" className="btn btn-danger">
+                                Delete
+                            </button>
+                        </form>
                     </div>
+                </div>
                 <hr />
             </main>
         </Def>
