399fc1f3897ca7f442116d2be165b22ffceaef37
NeNeZoRr <zorrillalin@gmail.com>
2023-11-15 12:40:36 -0500

    troubleshooting

--- models/index.js
+++ models/index.js
@@ -1,10 +1,12 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require("dotenv").config();
+const mongoose = require("mongoose");
+const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+const connect = async () => {
+  await mongoose.connect(MONGO_URI);
+  console.log("connected to Mongo");
+};
 
-module.exports.Place = require('./places')
-module.exports.Comment = require('./comment')
+module.exports = connect;
+module.exports.Place = require('./places');
+module.exports.Comment = require('./comment');
